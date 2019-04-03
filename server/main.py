e4323d592c158fd3cabdd2b462c1c0db356da9aa
Zachary Figueroa <zach44zach@gmail.com>
2019-04-03 00:22:45 -0400

    Added flask_login

--- server/main.py
+++ server/main.py
@@ -1,12 +1,19 @@
 import flask
 from flask_socketio import SocketIO, send, emit
 from flask_session import Session
+import flask_login
 
 app = flask.Flask("__main__")
 app.config['SECRET_KEY'] = 'avraerbaweg23t2fe'
-# app.config['SESSION_TYPE'] = 'filesystem'
-# Session(app)
-socketio = SocketIO(app)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+socketio = SocketIO(app, manage_session=False)
+
+
+# flask-login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 @app.route("/")
 def my_index():
