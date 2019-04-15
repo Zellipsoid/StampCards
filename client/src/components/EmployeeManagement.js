ebc101c470b5d4129867e186401114f48f07eba5
Zachary Figueroa <zach44zach@gmail.com>
2019-04-15 10:22:24 -0400

    Trying to make table functions woooork

--- client/src/components/EmployeeManagement.js
+++ client/src/components/EmployeeManagement.js
@@ -23,26 +23,51 @@ class EmployeeManagement extends Component {
             username_requesting: this.props.user_data.username
         });
     }
+    getEmployeeStatus = (username) => {
+        let user = this.state.employees.filter(obj => {
+            return obj['0'] === username;
+        })
+        if (user['1'] == 3 || user['1'] == 2) {
+            console.log(`${username} is manager`)
+            return true;
+        }
+        else {
+            console.log(`${username} is not manager`)
+            return false;
+        }
+    }
+    changeEmployeeStatus = (username) => {
+        console.log(username);
+        let user = this.state.employees.filter(obj => {
+            return obj['0'] === username;
+        })
+        if (user['1'] === 1) {
+            user['1'] = 2;
+        } else if (user['1'] === 2) {
+            user['1'] = 1;
+        }
+    }
     render() {
         return (
             <div>
                 <Input fluid action='Add to Roster' placeholder='Username' />
-                <TableRow employees={this.state.employees} />
+                <EmployeeTable employees={this.state.employees} getEmployeeStatus={this.getEmployeeStatus} changeEmployeeStatus={this.changeEmployeeStatus} />
                 <Button onClick={this.props.close_management_panel} attached="bottom" fluid>Back to Scanner</Button>
             </div>
         );
     }
+
 }
-function TableRow(props) {
+function EmployeeTable(props) {
     const employees = props.employees;
     const employee_array = employees.map(employee =>
-        <Table.Row>
+        <Table.Row key={employee['0']}>
             <Table.Cell>{employee['0']}</Table.Cell> {/*username*/}
             <Table.Cell collapsing>
-                <Checkbox toggle label="Manager" />
+                <Checkbox toggle label="Manager" onChange={props.changeEmployeeStatus(employee['0'])} checked={props.getEmployeeStatus(employee['0'])} disabled={employee['1'] === 3} />
             </Table.Cell>
             <Table.Cell collapsing>
-                <Button>Remove Employee</Button>
+                <Button disabled={employee['1'] === 3} >Remove Employee</Button>
             </Table.Cell>
         </Table.Row>
     )
--- server/static/react/service-worker.js
+++ server/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.d065162c7b67e666ece6d5c43206d6fe.js"
+  "/static/react/precache-manifest.7ff67869f83a96c37f44f8311fdea374.js"
 );
 
 workbox.clientsClaim();
